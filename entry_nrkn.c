d48a424c4d8c3658f1ae108ad0ecbd095ab0cdf7
Nik <nrkn.com@gmail.com>
2024-08-14 10:51:07 +1200

    Basic tile system

--- entry_nrkn.c
+++ entry_nrkn.c
@@ -1,3 +1,40 @@
+const int tile_width = 8;
+
+int world_to_tile( float world ){
+  return world / (float)tile_width;
+}
+
+float tile_to_world( int tile ){
+  return (float)tile * (float)tile_width;
+}
+
+Vector2 round_v2_to_tile( Vector2 world_pos ){
+  return v2(
+    tile_to_world( world_to_tile( world_pos.x ) ),
+    tile_to_world( world_to_tile( world_pos.y ) )
+  );
+}
+
+bool almost_equals(float a, float b, float epsilon){
+  return fabs(a - b) <= epsilon;
+}
+
+bool animate_f32_to_target( float* value, float target, float delta_t, float rate ){
+  *value += ( target - *value ) * ( 1.0 - pow( 2.0f, -rate * delta_t ) );
+
+  if( almost_equals(*value, target, 0.001f ) ){
+    *value = target;
+    return true; // gottem
+  }
+
+  return false;
+}
+
+void animate_v2_to_target( Vector2* value, Vector2 target, float delta_t, float rate ){
+  animate_f32_to_target(&(value->x), target.x, delta_t, rate);
+  animate_f32_to_target(&(value->y), target.y, delta_t, rate);
+}
+
 typedef struct Sprite {
   Gfx_Image* img;
 } Sprite;
@@ -51,7 +88,7 @@ Entity* entity_create(){
     }
   }
 
-  assert(entity_found, "no more entities!");
+  assert(entity_found, "eee no ents");
 
   return entity_found;
 }
@@ -102,11 +139,32 @@ void draw_entity( Entity* entity ){
   xform = m4_translate(
     m4_scalar(1.0), v3(entity->pos.x, entity->pos.y, 0)
   );
+  
   draw_image_xform(
     sprite->img, xform, 
     v2(sprite->img->width, sprite->img->height), 
     COLOR_WHITE
   );	
+}
+
+Vector2 mouse_to_world() {
+  float mouse_x = input_frame.mouse_x;
+  float mouse_y = input_frame.mouse_y;
+  Matrix4 proj = draw_frame.projection;
+  Matrix4 view = draw_frame.view;
+  float window_w = window.width;
+  float window_h = window.height;
+
+  // mormalize mouse coords
+  float ndc_x = ( mouse_x / ( window_w * 0.5f ) ) - 1.0f;
+  float ndc_y = ( mouse_y / ( window_h * 0.5f ) ) - 1.0f;
+
+  // to world coords
+  Vector4 world_pos = v4( ndc_x, ndc_y, 0, 1 );
+  world_pos = m4_transform( m4_inverse( proj ), world_pos );
+  world_pos = m4_transform( view, world_pos );
+
+  return (Vector2){ world_pos.x, world_pos.y };
 }
 
 int entry(int argc, char **argv) {	
@@ -120,13 +178,21 @@ int entry(int argc, char **argv) {
 
   world = alloc( get_heap_allocator(), sizeof(World) );
 
-  float zoom = 8.0;
+  float zoom = 10.0;
+  Vector2 camera_pos = v2(0, 0);
 
   sprites[SPRITE_player] = (Sprite){ .img=load_data_image("data/player.png") };
   sprites[SPRITE_rock0] = (Sprite){ .img=load_data_image("data/rock-0.png") };
   sprites[SPRITE_rock1] = (Sprite){ .img=load_data_image("data/rock-1.png") };
   sprites[SPRITE_tree0] = (Sprite){ .img=load_data_image("data/tree-0.png") };
   sprites[SPRITE_tree1] = (Sprite){ .img=load_data_image("data/tree-1.png") };  
+
+  Gfx_Font *font = load_font_from_disk( 
+    STR( "data/GothamRoundedMedium_21022.ttf"), get_heap_allocator()
+  );
+  assert(font, "bad times with font");
+
+  const u32 font_height = 120;
 
   for( int i = 0; i < 10; i++ ){
     Entity* rock_en = entity_create();
@@ -141,6 +207,8 @@ int entry(int argc, char **argv) {
       get_random_float32_in_range(-100, 100),
       get_random_float32_in_range(-100, 100)
     );
+
+    rock_en->pos = round_v2_to_tile(rock_en->pos);
   }
 
   Entity* player_en = entity_create();  
@@ -163,6 +231,8 @@ int entry(int argc, char **argv) {
       get_random_float32_in_range(-100, 100),
       get_random_float32_in_range(-100, 100)
     );
+
+    tree_en->pos = round_v2_to_tile(tree_en->pos);
   }
 
   float64 seconds_counter = 0.0;
@@ -171,20 +241,36 @@ int entry(int argc, char **argv) {
 
 	while (!window.should_close) {
 		reset_temporary_storage();
-
-    draw_frame.projection = m4_make_orthographic_projection(
-      window.scaled_width * -0.5, window.scaled_width * 0.5, 
-      window.scaled_height * -0.5, window.scaled_height * 0.5, 
-      -1, 10
-    );
-
-    draw_frame.view = m4_make_scale(v3( 1.0 / zoom, 1.0 / zoom, 1.0));
-
+    
     float64 now = os_get_current_time_in_seconds();
     float64 delta_t = now - last_time;
     last_time = now;
 
-		os_update(); 
+    os_update(); 
+
+    // camera
+    {
+      Vector2 target_pos = player_en->pos;
+      
+      animate_v2_to_target(&camera_pos, target_pos, delta_t, 15.0f);
+
+      draw_frame.projection = m4_make_orthographic_projection(
+        window.width * -0.5, window.width * 0.5, 
+        window.height * -0.5, window.height * 0.5, 
+        -1, 10
+      );
+
+      draw_frame.view = m4_make_scale(v3( 1.0, 1.0, 1.0));
+
+      draw_frame.view = m4_mul(
+        draw_frame.view, 
+        m4_make_translation(v3(camera_pos.x, camera_pos.y, 0))
+      );
+
+      draw_frame.view = m4_mul(
+        draw_frame.view, m4_make_scale(v3( 1.0 / zoom, 1.0 / zoom, 1.0))
+      );
+    }
 
     if( is_key_just_pressed(KEY_ESCAPE) ){
       window.should_close = true;
@@ -209,7 +295,63 @@ int entry(int argc, char **argv) {
     player_en->pos = v2_add(
       player_en->pos, v2_mulf(input_axis, player_en->speed * delta_t)
     );
-    
+
+    Vector2 mouse_in_world = mouse_to_world();
+    int mouse_tile_x = world_to_tile(mouse_in_world.x);
+    int mouse_tile_y = world_to_tile(mouse_in_world.y);
+
+    int player_tile_x = world_to_tile(player_en->pos.x);
+    int player_tile_y = world_to_tile(player_en->pos.y);
+    int tile_radius_x = 40;
+    int tile_radius_y = 30;
+    int tile_left = player_tile_x - tile_radius_x;
+    int tile_right = player_tile_x + tile_radius_x;
+    int tile_top = player_tile_y - tile_radius_y;
+    int tile_bottom = player_tile_y + tile_radius_y;
+
+    for( int x = tile_left; x < tile_right; x++ ){
+      for( int y = tile_top; y < tile_bottom; y++ ){     
+        float x_pos = x * tile_width;
+        float y_pos = y * tile_width;
+        
+        Vector4 col = hex_to_rgba(0xf0f0f0ff);
+
+        if( (x + y) % 2 == 0 ){
+          col = hex_to_rgba(0xfafafaff);
+        }
+
+        if( x == mouse_tile_x && y == mouse_tile_y ){
+          col = hex_to_rgba(0x3399ffff);
+        }
+
+        draw_rect( v2(x_pos, y_pos), v2(tile_width, tile_width), col );        
+      }
+    }
+
+    for( int i = 0; i < MAX_ENTITY_COUNT; i++ ){
+      Entity* en = &world->entities[i];
+
+      if( !en->is_valid ){
+        continue;
+      }
+
+      Sprite* sprite = get_sprite(en->sprite_id);
+      Vector2 sprite_size = v2(sprite->img->width, sprite->img->height);
+      Vector2 sprite_pos = v2_add(en->pos, v2(sprite_size.x * 0.5, 0));
+      Range2f bounds = range2f_make_bottom_center(sprite_size);
+      bounds = range2f_shift(bounds, sprite_pos);
+      
+      Vector4 col = hex_to_rgba(0x3399ffff);
+
+      col.a = 0;
+
+      if( range2f_contains(bounds, mouse_in_world) ){
+        col.a = 0.75;
+      }
+      
+      draw_rect(bounds.min, range2f_size( bounds ), col );
+    }
+
     for( int i = 0; i < MAX_ENTITY_COUNT; i++ ){
       Entity* en = &world->entities[i];
 
@@ -219,6 +361,12 @@ int entry(int argc, char **argv) {
 
       draw_entity(en);
     }
+
+    // draw_text( 
+    //   font, 
+    //   sprint( temp_allocator, STR("%f %f"), mouse_in_world.x, mouse_in_world.y), 
+    //   font_height, mouse_in_world, v2( 0.01f, 0.01f), COLOR_BLUE
+    // );
 
 		gfx_update();
 
